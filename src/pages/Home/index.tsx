88a9814afff13ce78e2c326521b2eedeae732d0f
KanBei <kanbei0605@gmail.com>
2022-12-14 08:41:29 -0500

    updated footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@material-tailwind/react";
 
 const Home: React.FC = () => {
   return (
-    <div className="container text-center p-8">
+    <div className="text-center p-8 bg-navy-blue1">
       <p className="text-blue-600 text-4xl font-bold">Widget</p>
     </div>
   );
