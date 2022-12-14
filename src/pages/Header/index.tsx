c3ed4f8161135d8fb6d67ee827d78e669d22b15c
KanBei <kanbei0605@gmail.com>
2022-12-14 02:45:07 -0500

    first commit

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-tailwind/react";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+			<Button>Button</Button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-tailwind/react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <p className="text-blue-600 text-4xl">HOME</p>
+    </div>
+  );
+};
+
+export default Home;
