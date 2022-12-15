590c6593857296e24f2f73609c79fb45a961ad58
KanBei <kanbei0605@gmail.com>
2022-12-15 15:42:14 -0500

    updated responsive navbar

--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Menu,
   MenuHandler,
   MenuList,
   MenuItem,
 } from "@material-tailwind/react";
-import MenuIcon from "../../components/Icons/MenuIcon";
+import { useLocation } from "react-router-dom";
+import { MenuIcon, CloseIcon } from "../../components/Icons";
 
 const NavbarComponent: React.FC = () => {
+  const [showResponsiveBar, setShowResponsiveBar] = useState<boolean>(false);
+  const location = useLocation();
   const headItems = [
     {
       href: "/nba",
@@ -30,7 +33,6 @@ const NavbarComponent: React.FC = () => {
       title: "WNBA",
     },
   ];
-  
   const menuItem = [
     {
       href: "/home",
@@ -64,7 +66,15 @@ const NavbarComponent: React.FC = () => {
           className="sm:h-12 h-10"
           alt=""
         />
-        <div className="absolute right-0">
+        <div className="absolute left-0 block sm:hidden">
+          <button
+            className="text-black bg-white font-bold p-2 rounded"
+            onClick={() => setShowResponsiveBar(!showResponsiveBar)}
+          >
+            <MenuIcon width={24} height={24}></MenuIcon>
+          </button>
+        </div>
+        <div className="absolute right-0 hidden sm:block">
           <Menu
             animate={{
               mount: { y: 0 },
@@ -79,7 +89,10 @@ const NavbarComponent: React.FC = () => {
             <MenuList className="bg-midenite-blue text-white">
               {menuItem.map((val, key) => {
                 return (
-                  <MenuItem className="hover:bg-[#ffffff12] hover:text-white" key={key}>
+                  <MenuItem
+                    className="hover:bg-[#ffffff12] hover:text-white"
+                    key={key}
+                  >
                     <a href={val.href} className="flex items-center">
                       {val.title}
                     </a>
@@ -90,19 +103,50 @@ const NavbarComponent: React.FC = () => {
           </Menu>
         </div>
       </div>
-      <div className="flex md:justify-center justify-start p-2 gap-x-16 overflow-auto bg-midenite-blue">
-        {headItems.map((val, key) => {
-          return (
-            <div
-              className="text-white font-bold hover:text-blue-700 p-1"
-              key={key}
-            >
-              <a href={val.href} className="flex items-center">
-                {val.title}
-              </a>
-            </div>
-          );
-        })}
+      <div className="hidden sm:block">
+        <div className="flex sm:justify-center justify-start p-2 gap-x-16 overflow-auto bg-midenite-blue">
+          {headItems.map((val, key) => {
+            return (
+              <div
+                className="text-white font-bold hover:text-blue-700 p-1"
+                key={key}
+              >
+                <a href={val.href} className="flex items-center">
+                  {val.title}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        className={`transform w-60 z-40 h-full absolute top-0 px-2 bg-midenite-blue shadow md:h-full flex-col justify-between sm:hidden transition duration-150 ease-in-out ${
+          showResponsiveBar ? "" : "-translate-x-60"
+        }`}
+      >
+        <div className="flex justify-end my-4">
+          <button
+            className="cursor-pointer"
+            onClick={() => setShowResponsiveBar(!showResponsiveBar)}
+          >
+            <CloseIcon width={25} height={25} color="#FFFFFF" />
+          </button>
+        </div>
+        <div className="flex flex-col">
+          <div className="text-gray-300 text-left font-semibold bg-[#ffffff12] py-2">
+            Menu
+          </div>
+          {menuItem.map((val, key) => {
+            return (
+              <div
+                className="text-gray-300 text-left font-semibold py-2 ml-4 cursor-pointer"
+                key={key}
+              >
+                <a href={val.href}>{val.title}</a>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
