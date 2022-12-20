96260906db020bd5696c96e7f3160ced8f7e6159
KanBei <kanbei0605@gmail.com>
2022-12-20 10:44:04 -0500

    updated privacy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useNavigate } from "react-router";
 import Footer from "./pages/Footer";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
--- src/pages/NBA/index.tsx
+++ src/pages/NBA/index.tsx
@@ -1,4 +1,3 @@
-// import "https://api.quarter4.io/american-football/widget/embed/161b7887-e6c0-4445-ba31-658e37076e3f/v1.js";
 import { useEffect } from "react";
 import { Widgets } from "./Widget.js";
 
@@ -9,7 +8,6 @@ const NBAPage: React.FC = () => {
       "https://api.quarter4.io/american-football/widget/embed/161b7887-e6c0-4445-ba31-658e37076e3f/v1.js";
     script.async = true;
     script.charset = "utf-8";
-		script.crossOrigin = "anonyouse";
     window.document.body.appendChild(script);
   }, []);
 
@@ -26,26 +24,6 @@ const NBAPage: React.FC = () => {
           </div>
         );
       })}
-      <blockquote
-        className="q4-game"
-        data-detail="true"
-        data-color-background="01005F"
-        data-color-text="FFFFFF"
-        data-color-high="FFFFFF"
-        data-color-medium="FFFFFF"
-        data-color-low="FFFFFF"
-        data-sport="american-football"
-        data-q4="6ec3f0aa-c85c-458e-bee1-d97204f644e1"
-      >
-        <span>
-          The San Francisco 49ers have a forecast 51% chance to win against The
-          Seattle Seahawks with a spread of -0.5/0.5 and an over/under of 40.5.
-          The San Francisco 49ers are 1 - 0 against The Seattle Seahawks in the
-          2022-23 Season.
-        </span>
-        &mdash;{" "}
-        <em>Thursday, December 15th San Francisco 49ers @ Seattle Seahawks</em>
-      </blockquote>
     </div>
   );
 };
