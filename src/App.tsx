bdf64da2478b18aec666565103d61883abfc3476
KanBei <kanbei0605@gmail.com>
2022-12-21 14:42:51 -0500

    added about page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import NBAPage from "./pages/NBA";
 import PrivacyPage from "./pages/Privacy";
 import CookiePage from "./pages/Cookie";
+import AboutPage from "./pages/About";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/nba" element={<NBAPage />}></Route>
         <Route path="/privacy" element={<PrivacyPage />}></Route>
         <Route path="/cookie" element={<CookiePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
       </Routes>
       <Footer />
     </>
---
+++ src/pages/About/index.tsx
@@ -0,0 +1,17 @@
+const AboutPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-2 sm:mt-28 mt-14">
+      <p className="text-xl font-bold">
+        Moneyline Sports is a multimedia platform and brand purpose built to
+        take the FOMO out of sports highlights and betting for fans everywhere.
+        The Moneyline Sports platform seeks to guide fans and sports bettors
+        alike through the best in class odds, stats and predictive analysis on
+        most professional sports. Our management team is made up of both
+        professional athletes, tech gurus and most importantly, fans just like
+        you!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
