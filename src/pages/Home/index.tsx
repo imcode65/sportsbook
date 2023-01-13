906f5b813e961c939be47c56d6be1104e5723ea5
KanBei <kanbei0605@gmail.com>
2023-01-13 12:09:39 -0500

    updated responsive

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ const Home: React.FC = () => {
       )
       .then((res) => {
         setFeeds(res.data.items);
-        console.log(res.data.feed);
+        console.log(res.data);
         setData(res.data.feed);
       })
       .catch((err) => {
@@ -33,8 +33,8 @@ const Home: React.FC = () => {
 
   return (
     <div className="p-12 mt-20">
-      <div className="grid grid-cols-5 gap-4 my-4">
-        <div className="col-span-3">
+      <div className="grid 2xl:grid-cols-5 grid-cols-1 gap-4 my-4 p-4">
+        <div className="col-span-3 p-4">
           <img
             width="100%"
             src="https://frontofficesports.com/wp-content/uploads/2023/01/FOS-PM-23-1.10-NFL-Sponsor-Revenue-1024x683.jpg"
@@ -44,15 +44,15 @@ const Home: React.FC = () => {
         <div className="col-span-2">
           {feeds.map((val, key) => {
             return (
-              <Card key={key} className="mb-10 p-4 static">
-                <div className="flex p-4">
-                  <img className="w-48" src={val.enclosure.link} alt="" />
+              <Card key={key} className="mb-8 p-4 static">
+                <div className="sm:flex text-center sm:text-left p-4">
+                  <img className="w-52 h-full" src={val.enclosure.link} alt="Not found img" />
                   {/* <img
-                    className="w-48"
+                    className="w-52 h-full"
                     src="https://frontofficesports.com/wp-content/uploads/2023/01/FOS-PM-23-1.10-NFL-Sponsor-Revenue-1024x683.jpg"
                     alt=""
                   /> */}
-                  <div className="ml-4">
+                  <div className="ml-4 h-32 overflow-hidden">
                     <p
                       className="text-xl hover:underline cursor-pointer text-black"
                       onClick={() => {
@@ -71,7 +71,7 @@ const Home: React.FC = () => {
       </div>
       <div className="bg-black px-4 py-8">
         <p className="text-white text-3xl text-bold">Featured</p>
-        <div className="flex gap-4 border-t-2 border-white py-4">
+        <div className="flex flex-wrap gap-4 border-t-2 border-white py-4">
           <img
             src="https://frontofficesports.com/wp-content/uploads/2022/12/FOS-22-12.27-Tom-Brady-Earnings-300x200.jpg"
             alt=""
